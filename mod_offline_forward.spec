35bbf4cb0a925e1b234b9f34a90da269e0a9aa48
Javad Mammadbayli <jmemmed@gmail.com>
2020-09-05 12:37:10 +0400

    Inital commit

---
+++ mod_offline_forward.spec
@@ -0,0 +1,5 @@
+author: "Mammadbayli"
+category: "http"
+summary: "Forward offline messages to HTTP endpoint"
+home: "https://mammadbayli.com"
+url: "https://github.com/Mammadbayli/mod_offline_forward.git"
---
+++ src/mod_offline_forward.erl
@@ -0,0 +1,62 @@
+-module(mod_offline_forward).
+
+-behaviour(gen_mod).
+ 
+-export([start/2, stop/1, mod_options/1, depends/2, create_message/1]).
+ 
+-include("scram.hrl").
+-include("xmpp.hrl").
+-include("logger.hrl").
+ 
+start(_Host, _Opt) ->
+	?INFO_MSG("starting mod_offline_forward", []),
+	inets:start(),
+	?INFO_MSG("HTTP client started", []),
+	ejabberd_hooks:add(offline_message_hook, _Host, ?MODULE, create_message, 50).  
+ 
+stop (_Host) ->
+	?INFO_MSG("stopping mod_offline_forward", []),
+	ejabberd_hooks:delete(offline_message_hook, _Host, ?MODULE, create_message, 50).
+
+depends(_Host, _Opts) ->
+    [].
+
+mod_options(_Host) ->
+    [].
+ 
+create_message({Action, Packet} = Acc) when (Packet#message.type == chat) ->
+		Type = fxml:get_path_s(xmpp:encode(Packet), [{elem,list_to_binary("data")}, {attr, list_to_binary("type")}]),
+
+		% Body = "",
+	    case Type of
+           <<"text">> ->  
+			   [{text, _, Body}] = Packet#message.body;
+           <<"audio">> -> 
+			   Body = list_to_binary("Voice");
+		   <<"photo">> -> 
+			   Body = list_to_binary("Photo")
+        end,
+
+		?INFO_MSG("Message XML ~p ~n",[Type]),
+        post_offline_message(Packet#message.from, Packet#message.to, Type, Body, Packet#message.id),
+  Acc;
+
+create_message(Acc) ->
+  Acc.
+
+post_offline_message(From, To, Type, Body, MessageId) ->
+  ?INFO_MSG("Posting From ~p To ~p Body ~p ID ~p~n",[From, To, Body, MessageId]),
+  ToUser = To#jid.luser,
+  FromUser = From#jid.luser,
+%   Vhost = To#jid.lserver,
+  Data = string:join(["{", 
+	  "to: ", binary_to_list(ToUser),
+	  ",from: ", binary_to_list(FromUser),
+	%   ",vhost: ", binary_to_list(Vhost),
+      ",type: ", binary_to_list(Type),
+      ",body: ", binary_to_list(Body),
+      ",messageId: ", binary_to_list(MessageId),
+  "}"], ""),
+  Request = {"https://ejabberd.free.beeceptor.com", [{"Authorization", "secrettoken"}], "application/json", Data},
+  httpc:request(post, Request,[],[]),
+  ?INFO_MSG("post request sent", []).
