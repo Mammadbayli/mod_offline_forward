5a90ecfedcf4f982dc708062e761dc03ff287d05
Javad Mammadbayli <jmemmed@gmail.com>
2022-02-22 13:58:02 +0400

    remove old logic

--- src/mod_offline_forward.erl
+++ src/mod_offline_forward.erl
@@ -29,65 +29,35 @@ stop (_Host) ->
 depends(_Host, _Opts) ->
     [].
 
+mod_doc() ->
+    #{desc =>
+            ?T("Forward offline messages to HTTP endpoint.")}.
+
 mod_options(_Host) ->
   [{auth_token, <<"secret">>},
   {remote_url, <<"http://example.com/test">>}].
 
 mod_opt_type(auth_token) ->
   fun iolist_to_binary/1;
+
 mod_opt_type(remote_url) ->
   fun iolist_to_binary/1.
 
-mod_doc() ->
-    #{desc =>
-          ?T("Forward offline messages to HTTP endpoint.")}.
-
 create_message({Action, Packet} = Acc) when (Packet#message.type == chat) ->
-
-    To = binary_to_list((Packet#message.to)#jid.luser),
-    From = binary_to_list((Packet#message.from)#jid.luser),
     Vhost = (Packet#message.to)#jid.lserver,
     BadgeCount = mod_offline:get_queue_length((Packet#message.to)#jid.luser, Vhost),
-
     forward_message(Packet, BadgeCount),
-
-    Type = fxml:get_path_s(xmpp:encode(Packet), [{elem,list_to_binary("data")}, {attr, list_to_binary("type")}]),
-
-    case Type of
-        <<"text">> ->
-	        [{text, _, Body}] = Packet#message.body,
-            post_offline_message(From, To, binary_to_list(Body), BadgeCount);
-        <<"audio">> -> 
-	        Body = <<"🎤 Audio"/utf8>>,
-            post_offline_message(From, To, binary_to_list(Body), BadgeCount);
-	    <<"photo">> -> 
-	        Body = <<"📷 Photo"/utf8>>,
-            post_offline_message(From, To, binary_to_list(Body), BadgeCount);
-	    <<"post">> -> 
-	        Body = <<"Shared a post"/utf8>>,
-            post_offline_message(From, To, binary_to_list(Body), BadgeCount);
-	    _ -> 
-            Packet
-    end,
-    
     Acc;
 
 create_message({Action, Packet} = Acc) ->
     case misc:unwrap_mucsub_message(Packet) of
         #message{} = Msg ->
-	        [{text, _, Body}] = Msg#message.body,
-	        User = binary_to_list((Msg#message.from)#jid.user),
-	        Resource = binary_to_list((Msg#message.from)#jid.resource),
-            To = binary_to_list((Msg#message.to)#jid.luser),
-	        From = string:join([Resource, User],"@"),
-	        Vhost = (Msg#message.to)#jid.lserver,
+            Vhost = (Msg#message.to)#jid.lserver,
             BadgeCount = mod_offline:get_queue_length((Msg#message.to)#jid.luser, Vhost),
-
-            post_offline_message(From, To, binary_to_list(Body), BadgeCount);
-	_ ->
+            forward_message(Packet, BadgeCount);
+	    _ ->
             Packet
     end,
-
     Acc;
 
 create_message(Acc) ->
@@ -108,17 +78,3 @@ forward_message(Packet, BadgeCount) ->
 
     Request = {binary_to_list(RemoteUrl), [{"Authorization", binary_to_list(Token)}], "application/json", Data},
     httpc:request(post, Request,[],[]).
-
-post_offline_message(From, To, Body, BadgeCount) ->
-    ?INFO_MSG("Posting From ~p To ~p Body ~p ID ~p~n",[From, To, Body, BadgeCount]),
- 
-    Data = string:join(["{",
-        "\"to\": \"", To, "\", ",
-        "\"from\": \"", From, "\", ",
-        "\"body\": \"", Body, "\", ",
-        "\"badge\": \"", integer_to_list(BadgeCount), "\"",
-    "}"], ""),
-  
-    Request = {string:join([os:getenv("NFS_API_URL"),"/notify"], ""), [{"Authorization", os:getenv("NFS_API_KEY")}], "application/json", Data},
-    httpc:request(post, Request,[],[]),
-    ?INFO_MSG("post request sent", []).
